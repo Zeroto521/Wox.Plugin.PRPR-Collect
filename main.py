1e10dab3db39546e78a3c4668c0032963f08f09b
Zero <zeroto521@gmail.com>
2019-07-21 20:21:09 +0800

    :hammer: pack script as project

---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from collect import Main
+
+if __name__ == '__main__':
+    Main()
---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from collect import Main
+
+if __name__ == '__main__':
+    t = Main()
+    res = t.query('')
+    print(res)
